6b1e45ed2aa24aef1b54ea3d03e3ebdf6aada026
warsier <787012293@qq.com>
2019-11-19 23:18:32 -0500

    update runpimprof

--- runpimprof.sh
+++ runpimprof.sh
@@ -1,5 +1,11 @@
+export CONFIGNAME=defaultconfig_16
 for bm in blackscholes bodytrack ferret fluidanimate freqmine raytrace swaptions vips x264
 do
-    export OMP_NUM_THREADS=1 && ./bin/parsecmgmt -a run -p ${bm} -c pimprof -n 1 -i simlarge -s "/home/warsier/Documents/pin-3.11/pin -t /home/warsier/Documents/PIMProf/build/PinInstrument/PinInstrument.so -c /home/warsier/Documents/PIMProf/Configs/defaultconfig.ini -o ${bm}.2.out --" &
+    export OMP_NUM_THREADS=1 && ./bin/parsecmgmt -a run -p ${bm} -c pimprof -n 1 -i simlarge -s "/home/warsier/Documents/pin-3.11/pin -t /home/warsier/Documents/PIMProf/build/PinInstrument/PinInstrument.so -c /home/warsier/Documents/PIMProf/Configs/${CONFIGNAME}.ini -o /home/warsier/Documents/PIMProfdata/parsec-pimprof/${bm}_${CONFIGNAME}.out --" > /home/warsier/Documents/PIMProfdata/parsec-pimprof/${bm}_${CONFIGNAME}.log 2>&1 &
+# the submit (-s) of parsec needs to be feeded as a parameter
+done
+for bm in canneal dedup streamcluster
+do
+    export OMP_NUM_THREADS=1 && ./bin/parsecmgmt -a run -p ${bm} -c pimprof -n 1 -i simlarge -s "/home/warsier/Documents/pin-3.11/pin -t /home/warsier/Documents/PIMProf/build/PinInstrument/PinInstrument.so -c /home/warsier/Documents/PIMProf/Configs/${CONFIGNAME}.ini -o /home/warsier/Documents/PIMProfdata/parsec-pimprof/${bm}_${CONFIGNAME}.out --" > /home/warsier/Documents/PIMProfdata/parsec-pimprof/${bm}_${CONFIGNAME}.log 2>&1 &
 # the submit (-s) of parsec needs to be feeded as a parameter
 done
