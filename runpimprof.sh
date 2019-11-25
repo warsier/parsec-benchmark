4c8c87db2a956a988fc5cb47820a6397aaf7ec3e
warsier <787012293@qq.com>
2019-11-25 02:43:54 -0500

    add new configurations for offloader injection

--- runpimprof.sh
+++ runpimprof.sh
@@ -1,4 +1,4 @@
-export CONFIGNAME=defaultconfig_16
+export CONFIGNAME=defaultconfig_32
 for bm in blackscholes bodytrack ferret fluidanimate freqmine raytrace swaptions vips x264 canneal dedup streamcluster
 do
     export OMP_NUM_THREADS=1 && ./bin/parsecmgmt -a run -p ${bm} -c pimprof -n 1 -i simlarge -s "/home/warsier/Documents/pin-3.11/pin -t /home/warsier/Documents/PIMProf/build/PinInstrument/PinInstrument.so -c /home/warsier/Documents/PIMProf/Configs/${CONFIGNAME}.ini -o /home/warsier/Documents/PIMProfdata/parsec-pimprof/${bm}_${CONFIGNAME}.out --" > /home/warsier/Documents/PIMProfdata/parsec-pimprof/${bm}_${CONFIGNAME}.log 2>&1 &
---
+++ runpimprof2.sh
@@ -0,0 +1,6 @@
+export CONFIGNAME=defaultconfig_32
+for bm in blackscholes bodytrack ferret fluidanimate freqmine raytrace swaptions vips x264 canneal dedup streamcluster
+do
+    export OMP_NUM_THREADS=1 && ./bin/parsecmgmt -a run -p ${bm} -c pimprof2 -n 1 -i simsmall -s "/home/warsier/Documents/pin-3.11/pin -t /home/warsier/Documents/PIMProf/build/PinInstrument/PinInstrument.so -c /home/warsier/Documents/PIMProf/Configs/${CONFIGNAME}.ini -o /home/warsier/Documents/PIMProfdata/parsec-pimprof/${bm}_${CONFIGNAME}.out --" > /home/warsier/Documents/PIMProfdata/parsec-pimprof/${bm}_${CONFIGNAME}.log 2>&1 &
+# the submit (-s) of parsec needs to be feeded as a parameter
+done
