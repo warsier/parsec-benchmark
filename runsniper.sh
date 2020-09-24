c25c545f3656fabb4abde6887cc9849c24d9a5e2
warsier <787012293@qq.com>
2020-09-24 17:12:35 -0400

    update script

--- runsniper.sh
+++ runsniper.sh
@@ -1,8 +1,18 @@
 set -x
+
+TOPDIRNAME=/home/warsier/Documents/PIMProfdata/parsec-sniper
+TOPDIRCOUNT=$(find "${TOPDIRNAME}" -mindepth "1" -maxdepth "1" -type "d" | wc -l)
+TARGETDIR=${TOPDIRNAME}/${TOPDIRCOUNT}
+CORECOUNT=32
+CORETYPE=pim
+DATASET=simsmall
 # blackscholes bodytrack ferret fluidanimate freqmine raytrace swaptions vips x264 canneal dedup streamcluster
-for bm in blackscholes bodytrack ferret fluidanimate freqmine raytrace swaptions vips x264 canneal dedup streamcluster
+BENCHMARKS="blackscholes bodytrack ferret fluidanimate freqmine raytrace swaptions vips x264 canneal dedup streamcluster"
+
+mkdir ${TARGETDIR}
+for bm in ${BENCHMARKS}
 do
-    # ./bin/parsecmgmt -a run -p ${bm} -n 32 -i simlarge -s "/home/warsier/Documents/sniper_pim_prof/sniper/run-sniper -n 32 -c pimprof_pim -d /home/warsier/Documents/PIMProfdata/parsec-sniper/${bm}_pim --" > /home/warsier/Documents/PIMProfdata/parsec-sniper/${bm}_pim.log 2>&1 &
-    ./bin/parsecmgmt -a run -p ${bm} -n 1 -i simlarge -s "/home/warsier/Documents/sniper_pim_prof/sniper/run-sniper -n 1 -c pimprof_cpu -d /home/warsier/Documents/PIMProfdata/parsec-sniper/${bm}_cpu --" > /home/warsier/Documents/PIMProfdata/parsec-sniper/${bm}_cpu.log 2>&1 &
+    ./bin/parsecmgmt -a run -p ${bm} -n ${CORECOUNT} -i ${DATASET} -s "/home/warsier/Documents/sniper_pim_prof/sniper/run-sniper -n ${CORECOUNT} -c pimprof_${CORETYPE} -d ${TARGETDIR}/${bm}_${CORETYPE}_${CORECOUNT} --" > ${TARGETDIR}/${bm}_${CORETYPE}_${CORECOUNT}.log 2>&1 &
+    echo "./bin/parsecmgmt -a run -p ${bm} -n ${CORECOUNT} -i ${DATASET} -s /home/warsier/Documents/sniper_pim_prof/sniper/run-sniper -n ${CORECOUNT} -c pimprof_${CORETYPE} -d ${TARGETDIR}/${bm}_${CORETYPE}_${CORECOUNT} -- > ${TARGETDIR}/${bm}_${CORETYPE}_${CORECOUNT}.log 2>&1 &" >> ${TARGETDIR}/command.log
 # the submit (-s) of parsec needs to be feeded as a parameter
 done
