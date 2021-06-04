4054ebe657465306b850735566ca48433e016468
warsier <787012293@qq.com>
2021-06-04 16:19:52 -0400

    add vtune configs

--- runsniper.sh
+++ runsniper.sh
@@ -5,11 +5,12 @@ TOPDIRNAME=/home/warsier/Documents/PIMProfdata/parsec-sniper
 TOPDIRCOUNT=$(find "${TOPDIRNAME}" -mindepth "1" -maxdepth "1" -type "d" | wc -l)
 TARGETDIR=${TOPDIRNAME}/${TOPDIRCOUNT}
 CFGTYPE=pimprof_inj
-CORECOUNT=1
-CORETYPE=cpu
+CORECOUNT=16
+CORETYPE=pim
 DATASET=simlarge
 # blackscholes bodytrack ferret fluidanimate freqmine raytrace swaptions vips x264 canneal dedup streamcluster
 BENCHMARKS="blackscholes bodytrack ferret fluidanimate freqmine raytrace swaptions vips x264 canneal dedup streamcluster"
+#BENCHMARKS="bodytrack dedup"
 
 mkdir ${TARGETDIR}
 for bm in ${BENCHMARKS}
